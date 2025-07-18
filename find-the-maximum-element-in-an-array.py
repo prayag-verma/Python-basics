1c817fd5101500dceb2bf3d83f9246c7b96dc84a
Prayag Verma <prayagpverma@gmail.com>
2025-07-17 23:27:11 -0500

    Added more methods to find

--- find-the-maximum-element-in-an-array.py
+++ find-the-maximum-element-in-an-array.py
@@ -16,3 +16,22 @@ for i in range(len(ar)):
         print("Last element of the sorted array using a loop:", ar[i])
         break
 
+ar.sort(reverse=True)
+
+# Method 4: Simply printing the first element of the sorted array in descending order
+for i in range(len(ar)):
+    if i == 0:
+        print("First element of the sorted array in descending order:", ar[i])
+        break
+    
+ar2 = [3, 17, 2, 10, 8]
+
+my_ar =[]
+
+for i in range(len(ar2)):
+    for j in range(len(ar2)):
+        if ar2[i] > ar2[j + 1]:
+            my_ar.append(ar2[i])
+        else:
+            my_ar.append(ar2[j])
+print("Maximum element in the array using nested loops:",my_ar)
